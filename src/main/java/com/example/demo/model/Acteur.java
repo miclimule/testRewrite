0d3e85faafe319c2a35776207a9e658e8d8e3925
Milclimule <99533207+miclimule@users.noreply.github.com>
2023-04-03 12:23:20 +0300

    test

--- src/main/java/com/example/demo/model/Acteur.java
+++ src/main/java/com/example/demo/model/Acteur.java
@@ -2,4 +2,14 @@ package com.example.demo.model;
 
 public class Acteur {
 
+	public Acteur() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	private void test() {
+		// TODO Auto-generated method stub
+
+	}
+	
 }
